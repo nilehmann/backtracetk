f44d315676e44d3323cfcbec70a7c2346e789e57
Nico Lehmann <nico.lehmannm@gmail.com>
2023-05-02 14:52:28 -0700

    first impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let backtrace = backtracetk::parse(&input);
+
+    backtrace.render()?;
+
+    Ok(())
 }
